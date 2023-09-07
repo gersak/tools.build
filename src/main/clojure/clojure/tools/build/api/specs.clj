d3375c9521a0a2777e124a75d6aa52cadcd48de8
Robert Geršak <robi@neyho.com>
2023-09-07 11:57:10 +0200

    Include URL and File as valid paths for copying in clojure.tools.build.api.specs

--- src/main/clojure/clojure/tools/build/api/specs.clj
+++ src/main/clojure/clojure/tools/build/api/specs.clj
@@ -1,6 +1,17 @@
 (ns clojure.tools.build.api.specs
   (:require [clojure.spec.alpha :as s]))
 
+
+(defn file? [x] (instance? java.net.URL x))
+(defn url? [x] (instance? java.net.URL x))
+
 (s/def ::lib qualified-ident?)
-(s/def ::path string?)
-(s/def ::paths (s/coll-of string?))
+(s/def ::path (s/or
+                ::string string? 
+                ::url url?
+                ::file file?))
+(s/def ::paths (s/coll-of
+                 (s/or
+                   ::string string? 
+                   ::url url?
+                   ::file file?)))
